b3573acfd6762af333d4d1cac626df9f2b6775e3
surya-prabhat <suryaprabhat2002@gmail.com>
2025-08-12 18:32:57 +0530

    Learnt a lot, created nav

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signIn, signOut } from "@/app/auth"
+import "./NavBar.css"
+
+
+
+async function NavBar() {
+
+    const session = await auth()
+
+
+    return (
+        <>
+            <div className="navBar">
+                <Link className="logo" href="/">
+                    <Image src="/Group 5.png" alt="Surya-app-Logo" width={144} height={144}></Image>
+                </Link>
+
+                <div className="auth">
+                    {session ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span className="create">Create</span>
+                            </Link>
+
+                            <form action={async () => {
+                                "use server"
+                                await signOut()
+                            }}>
+                                <button className="sign-out" type="submit">Sign-Out</button>
+
+                                {/* <Link href={`/user/${session.id}`}>
+                                    <span>{session?.user?.name}</span>
+                                </Link> */}
+                            </form>
+
+                        </>
+
+                    ) : (
+                        <form action={async () => {
+                            "use server"
+                            await signIn("github")
+                        }}>
+                            <button className="sign-in" type="submit">Sign-In</button>
+                        </form>
+
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default NavBar;
