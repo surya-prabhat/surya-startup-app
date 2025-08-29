29288b89142b91799e8cdb44f74de0dd59bb3026
surya-prabhat <suryaprabhat2002@gmail.com>
2025-08-29 18:30:58 +0530

    vercel deployemnt done, need to fix mobile layout and god bless

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -27,11 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${spaceGroteck.variable} ${inter.variable} antialiased`}
       >
-          <NavBar />
-          {children}
+        <NavBar />
+        {children}
       </body>
     </html>
   );
